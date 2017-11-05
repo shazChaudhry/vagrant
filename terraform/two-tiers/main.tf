095f30f0a0383816ab09829ab477b1f0f1840863
Shahzad A. Chaudhry <shahzad_chaudhry@hotmail.com>
2017-11-05 16:31:14 +0000

    fixed a type that prevented database to start

--- terraform/two-tiers/main.tf
+++ terraform/two-tiers/main.tf
@@ -213,7 +213,7 @@ resource "aws_instance" "database" {
   #!/bin/bash
   yum update -y && yum install -y mysql55-server && service mysqld start
   /usr/bin/mysqladmin -u root password 'secret'
-  mysql -u root -psecret -e "create user 'root'@'%' identified by 'secret';" mysql
+  mysql -u root -psecret -e "create user 'root'@'%' identified by 'secret';"
   mysql -u root -psecret -e 'CREATE TABLE mytable (mycol varchar(255));' test
   mysql -u root -psecret -e "INSERT INTO mytable (mycol) values ('linuxacademythebest') ;" test
 HEREDOC
